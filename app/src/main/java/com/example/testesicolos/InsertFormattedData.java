c8d02d456850eda561b6d21af8278b96a4227939
GuilhermeSotti <57609957+GuilhermeSotti@users.noreply.github.com>
2022-07-04 09:09:33 -0300

    Leia o Readme.md

--- app/src/main/java/com/example/testesicolos/InsertFormattedData.java
+++ app/src/main/java/com/example/testesicolos/InsertFormattedData.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 
 enum ExampleData {
 
+    // 3 Tabela de Dados fictícios
     NAME("Fernando Barros", "Ligia Melo", "Jefferson Amaral", "Emerson Giovaneli"),
     EMAIL("fernando.barros@sicolos.com.br", "ligia.melo@sicolos.com.br",
                   "jefferson.amaral@sicolos.com.br", "emerson.giovaneli@sicolos.com.br"),
@@ -17,11 +18,11 @@ enum ExampleData {
     private static ArrayList<String> list;
 
     ExampleData(@NonNull String... list) {
-        getList().addAll(Arrays.asList(list));
+        getList().addAll(Arrays.asList(list)); // Transforma cada tabela eu um ArrayList<String>()
     }
 
     public ArrayList<String> getList(){
-        return list;
+        return list; // Retorna ArrayList da tabela desejada
     }
 }
 public class InsertFormattedData {
@@ -30,19 +31,21 @@ public class InsertFormattedData {
 
         ArrayList<String> result = new ArrayList<>();
         int index;
-        for (String list: getData("NAME")){
+        // Iniciando valores para o Loop
+        
+        for (String list: getData("NAME")){ // Separando cada dado da tabela (Cada vetor do Array("NAME") fará o loop separadamente)
             index = 0;
             if (list.isEmpty())
-                list = "NULL";
+                list = "NULL"; // Completando o resultado final caso o dado esteja nulo
             result.add(list + ";");
             result.add(getData("EMAIL").get(index) + ";");
-            result.add(getData("RESULT").get(index) + "||");
+            result.add(getData("RESULT").get(index) + "||"); // Fazendo o insert de todos dados com o mesmo id do vetor e adicionando os separadores ";" e "||"
             index++;
         }
-        Log.println(Log.INFO, "InsertFormattedData", "Result List: " + result);
+        Log.println(Log.INFO, "InsertFormattedData", "Result List: " + result); // print
     }
 
     private static ArrayList<String> getData(String data){
-        return ExampleData.valueOf(data).getList();
+        return ExampleData.valueOf(data).getList(); // função que seleciona a tabela dada no parâmetro e retorna em ArrayList<String>()
     }
 }
